21656c78fd997bed04c6732d8b6068496b661de1
Harry Yin <harry.d.yin.gpc@gmail.com>
2024-02-11 11:18:46 -0800

    Created three activation functions and policy forward with initial intialization of the policy gradient

--- Reinforcement/Categorical.py
+++ Reinforcement/Categorical.py
@@ -6,8 +6,39 @@ import matplotlib.pyplot as plt
 import threading
 import winsound
 import colorama
+from enum import Enum
+from functools import partial
 
 
 class PolicyGradient(object):
-    def __init__(self):
-        self.weight = []
+    def __init__(self, env, learning_rate, hid_layers, activation):
+        self.num_states = env.observation_space
+        self.num_actions = env.action_space
+        self.learning_rate = learning_rate
+        self.hidden_layers = hid_layers
+        self.model_init()
+        self.activation = activation
+    
+    class Activation(Enum):  
+        RELU = partial(lambda x: max(0, x))
+        ELU = partial(lambda x, alpha: x if x >= 0 else alpha * (np.exp(x) - 1))
+        SWISH = partial(lambda x: x / (1 + np.exp(-x)))
+    
+    def model_init(self):
+        self.model = {
+            1: np.random.randn(self.hidden_layers, self.num_states) / np.sqrt(self.num_actions) * self.learning_rate,
+            2: np.random.randn(self.num_actions, self.hidden_layers) / np.sqrt(self.hidden_layers) * self.learning_rate,
+        }
+    
+    def softmax(self, x):
+        x = x.astype(np.float64)
+        exp_x = np.exp(x - np.max(x))
+        return exp_x / np.sum(exp_x)
+        
+    def policy_forward(self, state, alpha=0):
+        hid = np.dot(self.model[1], state)
+        hid = self.activation(hid, alpha) if alpha else self.activation(hid)
+        logp = np.dot(self.model[2], hid)
+        prob = self.softmax(logp)
+        return prob, hid
+    
