ec9f47bc1aa5f3ab02852bf18ef0b9b854a60e60
Harry Yin <harry.d.yin.gpc@gmail.com>
2024-02-11 14:17:34 -0800

    Created policy_backwards for grad updates

--- Reinforcement/Categorical.py
+++ Reinforcement/Categorical.py
@@ -11,9 +11,9 @@ from functools import partial
 
 
 class PolicyGradient(object):
-    def __init__(self, env, learning_rate, hid_layers, activation):
-        self.num_states = env.observation_space
-        self.num_actions = env.action_space
+    def __init__(self, os, acp, learning_rate, hid_layers, activation):
+        self.num_states = os
+        self.num_actions = acp
         self.learning_rate = learning_rate
         self.hidden_layers = hid_layers
         self.model_init()
@@ -41,3 +41,18 @@ class PolicyGradient(object):
         logp = np.dot(self.model[2], hid)
         prob = self.softmax(logp)
         return prob, hid
+    
+    def discount_rewards(self, rewards):
+        discounted_reward = np.zeros_like(rewards, dtype=np.float64)
+        total_rewards = 0
+        for t in reversed(range(0, len(rewards))):
+            total_rewards = total_rewards * self.discount + rewards[t]
+            discounted_reward[t] = total_rewards
+        return discounted_reward
+    
+    def policy_backward(self, state_change, hid, gprob, alpha=0):
+        dW2 = np.dot(hid.T, state_change)
+        dh = np.dot(gprob, self.model[2])
+        dh = self.activation(dh, 0) if alpha else self.activation(dh)
+        dW1 = np.dot(dh.T, state_change)
+        return {1: dW1, 2: dW2}
